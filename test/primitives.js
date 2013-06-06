ad7d3e3ec337db0f90bca4960641ec356600fc4e
ajlopez <webmaster@ajlopez.com>
2013-06-06 19:34:15 -0300

    Primitives pick; Machine nth

--- test/primitives.js
+++ test/primitives.js
@@ -117,3 +117,19 @@ assert.equal(machine.length(), 3);
 assert.equal(machine.pop(), 2);
 assert.equal(machine.pop(), 1);
 assert.equal(machine.pop(), 3);
+
+// pick
+
+assert.ok(primitives.pick);
+assert.equal(typeof primitives.pick, "function");
+
+machine.push(1);
+machine.push(2);
+machine.push(3);
+machine.push(2);
+primitives.pick(machine);
+assert.equal(machine.length(), 4);
+assert.equal(machine.pop(), 1);
+assert.equal(machine.pop(), 3);
+assert.equal(machine.pop(), 2);
+assert.equal(machine.pop(), 1);
