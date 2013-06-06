23905b1bd1af24bb3c0ecc266ded1671bdf4407f
ajlopez <webmaster@ajlopez.com>
2013-06-06 07:07:08 -0300

    Primtives rot

--- test/primitives.js
+++ test/primitives.js
@@ -90,3 +90,16 @@ assert.equal(machine.pop(), 2);
 assert.equal(machine.pop(), 1);
 assert.equal(machine.pop(), 2);
 
+// rot
+
+assert.ok(primitives.rot);
+assert.equal(typeof primitives.rot, "function");
+
+machine.push(1);
+machine.push(2);
+machine.push(3);
+primitives.rot(machine);
+assert.equal(machine.length(), 3);
+assert.equal(machine.pop(), 1);
+assert.equal(machine.pop(), 3);
+assert.equal(machine.pop(), 2);
