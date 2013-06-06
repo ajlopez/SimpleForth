5fa83003e5f5d99594374628d3fef95fcc71eb6b
ajlopez <webmaster@ajlopez.com>
2013-06-06 06:59:31 -0300

    Primitives over

--- test/primitives.js
+++ test/primitives.js
@@ -53,4 +53,16 @@ assert.equal(machine.length(), 2);
 assert.equal(machine.pop(), 1);
 assert.equal(machine.pop(), 2);
 
+// over
+
+assert.ok(primitives.over);
+assert.equal(typeof primitives.over, "function");
+
+machine.push(1);
+machine.push(2);
+primitives.over(machine);
+assert.equal(machine.length(), 3);
+assert.equal(machine.pop(), 1);
+assert.equal(machine.pop(), 2);
+assert.equal(machine.pop(), 1);
 
