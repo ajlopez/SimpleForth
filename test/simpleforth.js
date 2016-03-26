6288ea68a314fe8111ed5c37b3be0cfcf8eb85b2
ajlopez <webmaster@ajlopez.com>
2016-03-26 10:10:25 -0300

    Refactor createForth to forth

--- test/simpleforth.js
+++ test/simpleforth.js
@@ -2,7 +2,7 @@
 var sf = require('..');
 
 exports['Simple compile and run'] = function (test) {   
-    var forth = sf.createForth();
+    var forth = sf.forth();
     var func = sf.compile("1 2 +", forth);
 
     func(forth);
