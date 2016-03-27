0ba0f1413b467c7f11069589dc8f10295e43aa30
ajlopez <webmaster@ajlopez.com>
2016-03-27 10:59:05 -0300

    Additional test, 1+ as word token in lexer

--- test/lexer.js
+++ test/lexer.js
@@ -28,6 +28,19 @@ exports['Next token'] = function (test) {
     test.equal(token, null);
 }
 
+exports['Operator 1+ as word'] = function (test) {
+    var lexer = sfl.lexer('1+');
+    var token = lexer.nextToken();
+
+    test.ok(token);
+    test.equal(token.type, TokenType.Word);
+    test.equal(token.value, '1+');
+
+    token = lexer.nextToken();
+
+    test.equal(token, null);
+}
+
 exports['Skip spaces'] = function (test) {
     var lexer = sfl.lexer('  dup   ');
 
