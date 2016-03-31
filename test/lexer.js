308f4bf90deaa43c658abb4648bec1d0ce8c5fb7
ajlopez <webmaster@ajlopez.com>
2016-03-31 09:07:25 -0300

    Lexer skips comment

--- test/lexer.js
+++ test/lexer.js
@@ -28,6 +28,19 @@ exports['Next token'] = function (test) {
     test.equal(token, null);
 }
 
+exports['Next token skipping comment'] = function (test) {
+    var lexer = sfl.lexer('( a comment ) dup');
+    var token = lexer.nextToken();
+
+    test.ok(token);
+    test.equal(token.type, TokenType.Word);
+    test.equal(token.value, 'dup');
+
+    token = lexer.nextToken();
+
+    test.equal(token, null);
+}
+
 exports['Operator 1+ as word'] = function (test) {
     var lexer = sfl.lexer('1+');
     var token = lexer.nextToken();
