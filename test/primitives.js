644d42a18765ec00cf71c864472dfbb54a563c77
ajlopez <webmaster@ajlopez.com>
2016-06-17 07:48:58 -0300

    Primitive 0<

--- test/primitives.js
+++ test/primitives.js
@@ -1023,3 +1023,23 @@ exports['or'] = function (test) {
     test.strictEqual(machine.pop(), false);
 }
 
+exports['0<'] = function (test) {
+    test.ok(primitives['0<']);
+    test.equal(typeof primitives['0<'], "function");
+
+    machine.push(1);
+    primitives['0<'](machine);
+    test.equal(machine.length(), 1);
+    test.strictEqual(machine.pop(), false);
+
+    machine.push(-1);
+    primitives['0<'](machine);
+    test.equal(machine.length(), 1);
+    test.strictEqual(machine.pop(), true);
+
+    machine.push(0);
+    primitives['0<'](machine);
+    test.equal(machine.length(), 1);
+    test.strictEqual(machine.pop(), false);
+}
+
