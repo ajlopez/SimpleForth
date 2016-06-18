1a96c6ed73420040086c2d918d6dd38fbc63ab2b
ajlopez <webmaster@ajlopez.com>
2016-06-18 09:39:07 -0300

    Primitive 0>

--- test/primitives.js
+++ test/primitives.js
@@ -1043,3 +1043,23 @@ exports['0<'] = function (test) {
     test.strictEqual(machine.pop(), false);
 }
 
+exports['0>'] = function (test) {
+    test.ok(primitives['0>']);
+    test.equal(typeof primitives['0>'], "function");
+
+    machine.push(1);
+    primitives['0>'](machine);
+    test.equal(machine.length(), 1);
+    test.strictEqual(machine.pop(), true);
+
+    machine.push(-1);
+    primitives['0>'](machine);
+    test.equal(machine.length(), 1);
+    test.strictEqual(machine.pop(), false);
+
+    machine.push(0);
+    primitives['0>'](machine);
+    test.equal(machine.length(), 1);
+    test.strictEqual(machine.pop(), false);
+}
+
